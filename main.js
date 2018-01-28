a5f8efc651486733b798c8fb53455b3f75f96b61
Armando Soriano <uriz5555@gmail.com>
2018-01-27 18:58:45 -0600

    added redirect
    New redirect to welcom page using a button as proof of concept

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
+app.post('/welcome', function (req, res) {
+  res.sendFile(__dirname + "/src/welcome.html")
+})
 
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+
+   console.log("Example app listening at http://localhost:%s", port)
 })
