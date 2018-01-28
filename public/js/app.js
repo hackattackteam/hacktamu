cc9f2b974eeb7669fdbcdc9d84c28a2129633a3a
Michael Osei <mosei27@live.com>
2018-01-28 07:58:00 -0600

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -130,9 +130,9 @@
       }
     });
 
-    idx = Math.floor(Math.random() * Math.floor(targetDistricts.length)) + 1
+    idx = Math.floor(Math.random() * Math.floor(518)) + 1
     targetDistricts[idx]["District"] = min;
-
+    idx = idx - 1;
     if (pastTotalP < totalP) {
       console.log("flipped district.");
       pastTotalP = totalP;
@@ -142,15 +142,15 @@
       L.polygon(targetDistricts[idx].geometry.coordinates[0].map(function(x){
           return [x[1],x[0]]
       }),{
-                 color:(targetDistricts[idx]["District"] == 1) ? "#00ff00" 
-                  : (targetDistricts[idx]["District"] === 2) ? "#ff0000" 
-                  : (targetDistricts[idx]["District"] === 3) ? "#0000ff"
+                 color:(targetDistricts[idx]["District"] == 1) ? "#007700" 
+                  : (targetDistricts[idx]["District"] === 2) ? "#770000" 
+                  : (targetDistricts[idx]["District"] === 3) ? "#000077"
                   : (targetDistricts[idx]["District"] === 4) ? "#6700aa" : "#000000", 
              }).addTo(map)
     } else {
       targetDistricts[idx]["District"] = max;
     }
 
-  }, 2000);
+  }, 500);
 
 })();
