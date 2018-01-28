3b53ff4948b35eec9759df9f36afa80e4e80fa83
Michael Osei <mosei27@live.com>
2018-01-28 07:20:15 -0600

    its working, omg its working

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,8 @@
 (function() {
-  var map = L.map('map').setView([47, -99], 6.8);
+  var map = L.map('map', {zoomControl: false}, null).setView([47, -99], 6.8);
   var geojson = {};
-  
-  var style = {
-    fillColor: "#00bb00",
-    weight:2,
-    opaccity: 1,
-    color: 'white',
-    dashArray: '3',
-    fillOpacity: 0.7
-  }
+  var isRunOnce = false;
+
 
   function highlightFeature(e) {
     var layer = e.target;
@@ -32,8 +25,6 @@
 
   function onEachFeature(feature, layer) {
     layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight
     });
   }
 
@@ -48,11 +39,34 @@
 
     // method that we will use to update the control based on feature properties passed
     info.update = function (props) {
-        this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-            : 'Hover over a state');
+        this._div.innerHTML = 
+        "<div class='container ui-wrap'>"
+        +"<p>"+""+"</p>"
+        +"</div>";
     };
+    info.addTo(map);
   }
+
+  function style(feature) {
+    return {
+      fillColor: (function() {
+        // if(!isRunOnce) {
+          feature["District"] = Math.floor(Math.random() * Math.floor(4)) + 1;
+          return (feature["District"] == 1) ? "#00ff00" 
+                  : (feature["District"] === 2) ? "#ff0000" 
+                  : (feature["District"] === 3) ? "#0000ff"
+                  : (feature["District"] === 4) ? "#6700aa" : "#000000";
+        // } else { 
+        //   isRunOnce = true;
+        // }
+      })(),
+      weight:2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    }
+  } 
 
   geojson = L.geoJson(window.data, {
     style: style,
@@ -60,7 +74,83 @@
   }).addTo(map);
 
   map.dragging.disable();
+  attachGui();
+  
+    console.log("calculating");
 
-info.addTo(map);
+  window.setInterval(function() {  
+    area1 = 0;
+    area2 = 0;
+    area3 = 0;
+    area4 = 0;
+    perimeter1 = 0;
+    perimeter2 = 0;
+    perimeter3 = 0;
+    perimeter4 = 0;
+    polsby1 = 0;
+    polsby2 = 0;
+    polsby3 = 0;
+    polsby4 = 0;
+    window.data.features.forEach(function(f){
+      if(f["District"] == 1){
+        area1+=f.properties["Area"];
+        perimeter1+=f.properties["Perimeter"];
+      }
+      if(f["District"] == 2){
+        area2+=f.properties["Area"];
+        perimeter2+=f.properties["Perimeter"];
+      }
+      if(f["District"] == 3){
+        area3+=f.properties["Area"];
+        perimeter3+=f.properties["Perimeter"];
+      }
+      if(f["District"] == 4){
+        area4+=f.properties["Area"];
+        perimeter4+=f.properties["Perimeter"];
+      }
+    });
+
+    polsby1 = (4*Math.PI*area1)/(Math.pow(perimeter1, 2));
+    polsby2 = (4*Math.PI*area2)/(Math.pow(perimeter2, 2));
+    polsby3 = (4*Math.PI*area3)/(Math.pow(perimeter3, 2));
+    polsby4 = (4*Math.PI*area4)/(Math.pow(perimeter4, 2));
+    
+    totalP = polsby1+polsby2+polsby3+polsby4;
+    
+    scores = [polsby1, polsby2, polsby3, polsby4];
+    console.log(scores);
+    max = scores.indexOf(Math.max(...scores)) + 1;
+    min = scores.indexOf(Math.min(...scores)) + 1;
+
+    pastTotalP = 0.0;
+    targetDistricts = [];
+    window.data.features.forEach(function(x) {
+      if (x['District'] === max) {
+        targetDistricts.push(x);
+      }
+    });
+
+    idx = Math.floor(Math.random() * Math.floor(targetDistricts.length)) + 1
+    targetDistricts[idx]["District"] = min;
+
+    if (pastTotalP < totalP) {
+      console.log("flipped district.");
+      pastTotalP = totalP;
+      console.log(pastTotalP);
+      console.log(targetDistricts[idx].geometry.coordinates[0]); 
+      //targetDistricts[idx].geometry.setStyle({fillColor: "#000"});
+      L.polygon(targetDistricts[idx].geometry.coordinates[0].map(function(x){
+          return [x[1],x[0]]
+      }),{
+                 color:(targetDistricts[idx]["District"] == 1) ? "#00ff00" 
+                  : (targetDistricts[idx]["District"] === 2) ? "#ff0000" 
+                  : (targetDistricts[idx]["District"] === 3) ? "#0000ff"
+                  : (targetDistricts[idx]["District"] === 4) ? "#6700aa" : "#000000", 
+             }).addTo(map)
+    } else {
+      targetDistricts[idx]["District"] = max;
+    }
+
+  }, 2000);
 
 })();
