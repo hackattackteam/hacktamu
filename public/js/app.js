df33627bcb073245f1be0ac7a8b4b77da87917f0
Michael Osei <mosei27@live.com>
2018-01-28 02:03:04 -0600

    Add leaflet map

--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,66 @@
-function landingPage() {
-  document.querySelector("#landing-page").style.display = "";
-  document.querySelector("#about-page").style.display = "none";
-  document.querySelector("#map-page").style.display = "none";
+(function() {
+  var map = L.map('map').setView([47, -99], 6.8);
+  var geojson = {};
+  
+  var style = {
+    fillColor: "#00bb00",
+    weight:2,
+    opaccity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+  }
 
-  document.querySelector("#home-li").classList.add('active');
-  document.querySelector("#about-li").classList.remove('active');
-  document.querySelector("#map-li").classList.remove('active');
-}
+  function highlightFeature(e) {
+    var layer = e.target;
 
-function aboutPage() {
-  document.querySelector("#landing-page").style.display = "none";
-  document.querySelector("#about-page").style.display = "";
-  document.querySelector("#map-page").style.display = "none";
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
 
-  document.querySelector("#home-li").classList.remove('active');
-  document.querySelector("#about-li").classList.add('active');
-  document.querySelector("#map-li").classList.remove('active');
-}
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+  }
 
-function mapPage() {
-  document.querySelector("#landing-page").style.display = "none";
-  document.querySelector("#about-page").style.display = "none";
-  document.querySelector("#map-page").style.display = "";
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
 
-  document.querySelector("#home-li").classList.remove('active');
-  document.querySelector("#about-li").classList.remove('active');
-  document.querySelector("#map-li").classList.add('active');
-}
+  function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+  }
 
-require([
-  "esri/Map",
-  "esri/layers/Layer",
-  "esri/views/MapView",
-  "dojo/domReady!"
-], function(Map, Layer, MapView){
-  var map = new Map({
+  function attachGui() {  
+    var info = L.control();
 
-  });
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
 
-  Layer.fromPortalItem({
-    portalItem: {id: "73d81e36cb9a4d71b265d04c7bc80759"}
-  }).then(function(lyr){
-  // Adds layer to the map
-  map.add(lyr);
-  });
+    // method that we will use to update the control based on feature properties passed
+    info.update = function (props) {
+        this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+            : 'Hover over a state');
+    };
+  }
 
-  var view = new MapView({
-    container: "map-page",  // Reference to the scene div created in step 5
-    map: map,  // Reference to the map object created before the scene
-    zoom: 3,  // Sets zoom level based on level of detail (LOD)
-    center: [-100, 47.5],  // Sets center point of view using longitude,latitude
-    ui: {
-        components: [ "attribution" ]
-      }
-  });
-  //Disable mouse zoom
-  view.on("mouse-wheel", function(evt){
-    evt.stopPropagation();
-  });
-  //Disable double-click zoom
-  view.on("double-click", function(evt){
-    evt.stopPropagation();
-  });
-  //Disable touch zoom
-  view.on("drag", function(evt){
-    evt.stopPropagation();
-  });
-});
+  geojson = L.geoJson(window.data, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
+  map.dragging.disable();
+
+info.addTo(map);
+
+})();
