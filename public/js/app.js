64cd9e32b74057d55c6d0be500eebf75011997e0
Armando Soriano <uriz5555@gmail.com>
2018-01-27 21:52:28 -0600

    centered map

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ require([
   "dojo/domReady!"
 ], function(Map, Layer, MapView){
   var map = new Map({
-    
+
   });
 
   Layer.fromPortalItem({
@@ -18,8 +18,8 @@ require([
   var view = new MapView({
     container: "viewDiv",  // Reference to the scene div created in step 5
     map: map,  // Reference to the map object created before the scene
-    zoom: 7,  // Sets zoom level based on level of detail (LOD)
-    center: [-100, 47],  // Sets center point of view using longitude,latitude
+    zoom: 3,  // Sets zoom level based on level of detail (LOD)
+    center: [-100, 47.5],  // Sets center point of view using longitude,latitude
     ui: {
         components: [ "attribution" ]
       }
