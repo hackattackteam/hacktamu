6e65b3bfcc428caad3a34572fcb8ed10c75d3546
Brandon Jurado <brandonjurado13@yahoo.com>
2018-01-27 22:15:33 -0600

    FIXED ARMANDOS MISTAKES AS USUAL

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,33 @@ function landingPage() {
   document.querySelector("#landing-page").style.display = "";
   document.querySelector("#about-page").style.display = "none";
   document.querySelector("#map-page").style.display = "none";
+
+  document.querySelector("#home-li").classList.add('active');
+  document.querySelector("#about-li").classList.remove('active');
+  document.querySelector("#map-li").classList.remove('active');
 }
 
 function aboutPage() {
   document.querySelector("#landing-page").style.display = "none";
   document.querySelector("#about-page").style.display = "";
   document.querySelector("#map-page").style.display = "none";
+
+  document.querySelector("#home-li").classList.remove('active');
+  document.querySelector("#about-li").classList.add('active');
+  document.querySelector("#map-li").classList.remove('active');
 }
 
 function mapPage() {
+  console.log('map displayed');
+  console.log(document.querySelector("#map-page").style.display);
   document.querySelector("#landing-page").style.display = "none";
   document.querySelector("#about-page").style.display = "none";
   document.querySelector("#map-page").style.display = "";
+  console.log(document.querySelector("#map-page").style.display);
+
+  document.querySelector("#home-li").classList.remove('active');
+  document.querySelector("#about-li").classList.remove('active');
+  document.querySelector("#map-li").classList.add('active');
 }
 
 require([
